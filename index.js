5bda65f3892946e2d8590521934342dbb428337b
aadarshpatel17 <kaadarsh173@gmail.com>
2020-11-02 10:44:41 +0530

    final

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 function loveCalcy() {
     var firstPerson = document.getElementById("firstPerson").value;
     var secondPerson = document.getElementById("secondPerson").value;
-
-    var size1 = firstPerson.length;
-    var size2 = secondPerson.length;
-
-    var percentage = 100 / size1;
     var result = 0;
 
-    if (size1 <= size2) {
-        for (var i = 0; i < size1; i++) {
+    if(firstPerson=="" || secondPerson=="")
+        alert("Please! Enter Name");
+
+
+    if (firstPerson.length <= secondPerson.length) {
+        for (var i = 0; i < firstPerson.length; i++) {
             if (secondPerson.includes(firstPerson[i]))
-                result += percentage;
+                result += (100 / firstPerson.length);
         }
     } else {
-        for (var i = 0; i < size2; i++) {
+        for (var i = 0; i < secondPerson.length; i++) {
             if (firstPerson.includes(secondPerson[i]))
-                result += percentage;
+            result += (100 / secondPerson.length);
         }
     }
     document.getElementById('love-percentage').innerHTML = Math.ceil(result);
